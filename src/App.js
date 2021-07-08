393b09bbd1da0c4bd7a88281ecd48f70afe52907
SARKAR <sshankhadeep226@gmail.com>
2021-07-08 11:01:54 +0530

    pathFinedr

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import PathFinder from './path_finder/pathFinder'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="head"><h1>WELCOME TO DIJKSTRA'S PATH FINDER</h1></div>
+     <PathFinder/>
     </div>
   );
 }
